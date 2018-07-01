e2ebcd0a66cd693e42663da28dbdc13e2f0e2ebb
Gregor Konzett <gregor.konzett98@gmail.com>
2018-07-01 21:53:52 +0200

    Add project files.

---
+++ KSP_KRPC_001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KSP_KRPC_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
